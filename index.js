8ea4fc0a71bfd48d86a0265bddeddc7a2a97cf3b
alanblee <alanbenlee12@gmail.com>
2020-11-24 18:17:15 -0800

    added func to check if the recurring issue already exists on the board

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { JiraConnector } = require("reshuffle-jira-connector");
 
 (async () => {
   const app = new Reshuffle();
+  // Cront config
+  const cronConnector = new CronConnector(app);
   // Jira Config
   const jira = new JiraConnector(app, {
     host: process.env.JIRA_HOST,
@@ -13,5 +15,27 @@ const { JiraConnector } = require("reshuffle-jira-connector");
     baseURL: process.env.RUNTIME_BASE_URL,
   });
 
+  const checkIssues = async () => {
+    const boardIssues = await jira.sdk().getIssuesForBoard(1);
+    for (const issue of boardIssues.issues) {
+      const { fields } = issue;
+      if (fields.summary === "I want this to be recurring") {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  cronConnector.on({ expression: "1 * * * * *" }, async (event, app) => {
+    // look for the issue summary, keep note of it if its there.
+    // if not, create new issue with same summary and keep note of its id
+    // get all the issues from the board
+    const found = await checkIssues();
+    if (found) {
+      console.log("found it");
+    } else {
+      console.log("nothing here create new issue");
+    }
+  });
   app.start(8000);
 })().catch(console.error);
